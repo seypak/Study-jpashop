3f43f48af83d0a7e3c482ef2b1dc45f8360479c4
seypark <seypak@naver.com>
2023-11-01 14:52:55 +0900

    Feat: 섹션 7. 웹 계층 개발 - 회원 목록 조회
    회원목록 조회 (GET)
    thymeleaf
    	th:each - th:each="member : ${members}"
    	th:text - th:text="${member.id}" ( EL문이랑 유사함 )
    			- th:text="${member.address.city}" ( . 으로 객체그래프를 뽑아올 수 있다. )

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -42,5 +43,21 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    /**
+     * 요구사항이 정말 단순할 때는 멤버 폼 없이 멤버 엔티티를 그대로 써도 되지만,
+     * 실무는 단순하지 않기때문에 화면 기능 때문에 엔티티가 결국 유지 보수하기가 어려워진다.
+     * 그래서 엔티티는 핵심 비즈니스 로직만 가지고 있고
+     * 화면을 위한 로직은 폼 객체나 DTO(Data Transfer Object) 을 사용해야한다.
+     * 또한, API를 만들 때는 이유를 불문하고 절대 엔티티를 넘기면 안된다.
+     * 보안적인 정보가 노출되는 문제도 있고 또 하나는 API 스펙이 변해버리는 문제가 있다.
+     * => 아래 예제는 테스트 코드이므로 단순하게 엔티티를 그대로 반환하였다.
+     */
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
